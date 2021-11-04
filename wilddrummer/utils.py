1f20467017b07ba347a780f56ee1ef4ec927e743
leopardShark <lamicccc@gmail.com>
2021-11-04 18:48:24 +0100

    Update utils.py

--- wilddrummer/utils.py
+++ wilddrummer/utils.py
@@ -1,50 +1,61 @@
 #! /usr/bin/env python3
 
 from pydub import AudioSegment
-from pydub.silence import split_on_silence
 import numpy as np
 import librosa
-import random
-from scipy.signal import find_peaks
+import noisereduce as nr
+import soundfile as sf
+
+
+def denoise(file):
+    """ denoise file """
+
+    data, sr = librosa.load(file)
+    denoised_data = nr.reduce_noise(y=data, sr=sr)
+    sf.write('denoised_file.wav', denoised_data, samplerate=sr)
+
+    return denoised_data, 'denoised_file.wav'
 
 
 def find_onsets(file):
-    """ librosa onset detection"""
+    """ librosa onset detection """
 
     y, sr = librosa.load(file)
     o_env = librosa.onset.onset_strength(y, sr=sr)
     onset_frames = librosa.onset.onset_detect(onset_envelope=o_env, sr=sr)
     onset_samples = list(librosa.frames_to_samples(onset_frames))
-    onset_arr = (np.array(onset_samples)/sr*1000).astype(int)
+    onset_arr = (np.array(onset_samples) / sr * 1000).astype(int)
 
     return onset_arr
 
 
 def make_beats(audio, sample_list, sample_rate, bpm):
     """ make beats from audio segments """
-    
-    bpms = bpm/60000
-    interval = 1/bpms#/4
+
+    bpms = bpm / 60000
+    interval = 1 / bpms  # /4
+    intro = 0
+    outro = 1000
 
     playlist = AudioSegment.empty()
     for s in sample_list:
-        start = s - 23
-        end = s + 23
+        start = s - intro
+        end = s + outro
         samp = audio[start:end]
-        enter = 23 * 2
-        interval_silence = AudioSegment.silent(duration=int(interval-enter))
-        playlist += interval_silence
+        enter = intro + outro
+        interval_silence = AudioSegment.silent(duration=int(interval - enter))
         playlist += samp
+        playlist += interval_silence
 
     return playlist
 
 
 file = 'kiki_0001.m4a'
-audio = AudioSegment.from_file(file)
+data, new_file = denoise(file)
+audio = AudioSegment.from_file(new_file)
 sample_rate = audio.frame_rate
 
-sample_arr = find_onsets(file)
-beats = make_beats(audio, sample_arr, sample_rate, 120)
+sample_arr = find_onsets(new_file)
+beats = make_beats(audio, sample_arr, sample_rate, 100)
 
 file_handle = beats.export("output.wav", format="wav")
-
