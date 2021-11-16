22162622470263e667b74a300489763ff5a78142
leopardShark <lamicccc@gmail.com>
2021-11-16 03:07:06 +0100

    Update wild_drummer.py

--- wild_drummer.py
+++ wild_drummer.py
@@ -40,35 +40,36 @@ def save_file():
         return render_template('content.html', content=content)
 
 
-@app.route('/generate_file', methods=['GET', 'POST'])
+@app.route('/output', methods=['GET','POST'])
 def generate_file():
+    if request.method == 'POST':
+        bpm = request.form.get("bpm", type=int)
+        meter = request.form.get("meter", type=int)
+        name = 'audio.wav'
+        file = app.config["UPLOAD_FOLDER"] + name
+        y, sr = denoise(file)
+        y_dub = np.array(y * (1 << 15), dtype=np.int16)
 
-    bpm = request.form.get("bpm", type=int)
-    meter = request.form.get("meter", type=int)
-    name = 'audio.wav'
-    file = app.config["UPLOAD_FOLDER"] + name
-    y, sr = denoise(file)
-    y_dub = np.array(y * (1 << 15), dtype=np.int16)
+        audio = AudioSegment(
+            y_dub.tobytes(),
+            frame_rate=sr,
+            sample_width=y_dub.dtype.itemsize,
+            channels=1) + 10
 
-    audio = AudioSegment(
-        y_dub.tobytes(),
-        frame_rate=sr,
-        sample_width=y_dub.dtype.itemsize,
-        channels=1) + 10
+        high_start, high_onsets, low_start, low_onsets = find_onsets(y, sr)
+        down_beats = make_beats(audio, high_onsets, bpm, high_start, outro=100)
+        up_beats = make_beats(audio, low_onsets, bpm, low_start, outro=100) - 10
+        gap = high_start[0] - low_start[0]
+        beats = mix_beats(down_beats, up_beats, bpm, meter, delay=gap)
+        beats_path = os.path.join(app.config["UPLOAD_FOLDER"], "output.wav")
+        beats.export(beats_path, format="wav")
+        beats_file = url_for('download_file', name="output.wav")
 
-    high_start, high_onsets, low_start, low_onsets = find_onsets(y, sr)
-    down_beats = make_beats(audio, high_onsets, bpm, high_start, outro=100)
-    up_beats = make_beats(audio, low_onsets, bpm, low_start, outro=100) - 10
-    gap = high_start[0] - low_start[0]
-    beats = mix_beats(down_beats, up_beats, bpm, meter, delay=gap)
-    beats_path = os.path.join(app.config["UPLOAD_FOLDER"], "output.wav")
-    beats.export(beats_path, format="wav")
-    beats_file = url_for('download_file', name="output.wav")
-
-    return render_template('output.html', output=beats_file)
+        return render_template('output.html', output=beats_file)
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000, debug=True)
-    # app.add_url_rule(
-    # "/uploads/<name>", endpoint="download_file", build_only=True)
+    #app.add_url_rule(
+    #    "/uploads/<name>", endpoint="download_file", build_only=True)
